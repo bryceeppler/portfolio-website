6c9c35ed77b0faec380715b7a29a56df6253c5a6
Bryce Eppler <eppler97@gmail.com>
2022-10-30 07:27:23 -0700

    initial layout change

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,15 +9,17 @@ module.exports = {
 			'xl': '1.25rem',
 			'2xl': '1.6rem',
 			'3xl': '2.2rem',
-			'4xl': '3rem',
+			'4xl': '4.5rem',
 			'5xl': '6rem',
 		},
+		// source code pro for mono and roboto for sans
 		fontFamily: {
-			sans: ['Titillium Web', 'sans-serif'],
+			'sans': ['Roboto', 'sans-serif'],
+			'mono': ['Source Code Pro', 'monospace'],
 		},
 		extend: {
 			colors: {
-				'steel-background': '#262D3A',
+				'steel-background': '#252630',
 			},
 		},
 	},
